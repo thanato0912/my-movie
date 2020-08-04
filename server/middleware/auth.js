ed825a28d9753f8c7eaf22a0ceee2982b4cce009
Sangmin Kim <kim305@usc.edu>
2020-08-04 20:37:22 +0900

    misc

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,6 @@
 const { User } = require('../models/users.model');
 
 let auth = (req, res, next) => {
-  console.log('hi');
   let token = req.cookies.x_auth;
   if (!token) return res.json({ loginSuccess: false });
   User.findByToken(token, (err, user) => {
