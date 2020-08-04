c5560e88eb546a85997f787c2fa9551c411c8597
Sangmin Kim <kim305@usc.edu>
2020-08-04 20:56:27 +0900

    misc

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
@@ -8,21 +8,23 @@ import MovieDetails from './components/MovieDetails/MovieDetails';
 import MyFavorites from './components/MyFavorites/MyFavorites';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
-import { Layout} from 'antd';
+import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 const { Footer } = Layout;
 
 function App() {
   return (
     <Router>
-      <NavBar/>
+      <NavBar />
       <Route exact path='/' component={Home} />
       <Route exact path='/latest' component={Latest} />
-      <Route exact path='/movie/:movieId' component={MovieDetails} />
+      <Route path='/movie/:movieId' component={MovieDetails} />
       <Route exact path='/favorites' component={MyFavorites} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
-      <Footer style={{ textAlign: 'center' }}>My-Movie ©2020 Created by SSSAANG</Footer>
+      <Footer style={{ textAlign: 'center' }}>
+        My-Movie ©2020 Created by SSSAANG
+      </Footer>
     </Router>
   );
 }
