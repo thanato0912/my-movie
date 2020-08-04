fda9595c1c977feb8ba9e1ca59ae83bc64f597c1
Sangmin Kim <kim305@usc.edu>
2020-08-04 13:55:07 +0900

    misc

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,9 @@
 export const movieAPIKey = '77ec013b8f3ebce20f6b6b185bc76d23';
-export const movieURI ='https://api.themoviedb.org/3/';
+export const movieURI = 'https://api.themoviedb.org/3/';
 export const imageURL = 'https://image.tmdb.org/t/p/';
+
+if (process.env.REACT_APP === 'development') {
+  export const BASE_URL = 'http://localhost:5000/';
+} else {
+  export const BASE_URL = '52.79.61.9:5000/';
+}
