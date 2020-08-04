ee1b8d4b4154d38b83e163058bb0a1845f429894
Sangmin Kim <kim305@usc.edu>
2020-08-04 17:30:14 +0900

    misc

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const userRouter = require('./routes/users.route');
 const favoriteRouter = require('./routes/favorites.route');
