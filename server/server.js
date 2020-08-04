e3be416a16a09c73dbdf0543fce85c18e47bb1b6
Sangmin Kim <kim305@usc.edu>
2020-08-04 20:44:56 +0900

    misc

--- server/server.js
+++ server/server.js
@@ -22,9 +22,6 @@ app.use('/users', userRouter);
 app.use('/favorites', favoriteRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
 }
 
 mongoose
