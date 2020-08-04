13725ce3bd42d117963a9cbc2e7b466a556afbc2
Sangmin Kim <kim305@usc.edu>
2020-08-04 17:56:18 +0900

    misc

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -15,7 +15,7 @@ function Login() {
     };
 
     axios
-      .post('/temp/users/login', user, {
+      .post('/users/login', user, {
         withCredentials: true,
         mode: 'cors',
       })
