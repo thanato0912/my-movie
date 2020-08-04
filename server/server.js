a342b179d105cdb842138b9a321ded6603c4a246
Sangmin Kim <kim305@usc.edu>
2020-08-04 20:58:56 +0900

    misc

--- server/server.js
+++ server/server.js
@@ -22,12 +22,8 @@ app.use('/users', userRouter);
 app.use('/favorites', favoriteRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  // });
-  // app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
