9a3047916becb49bd701d8fa7bab50a0c3c54500
Sangmin Kim <kim305@usc.edu>
2020-08-04 17:37:30 +0900

    misc

--- server/server.js
+++ server/server.js
@@ -24,8 +24,13 @@ app.use(cookieParser());
 app.use('/users', userRouter);
 app.use('/favorites', favoriteRouter);
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
 mongoose
   .connect(process.env.mongoURI, {
     useNewUrlParser: true,
