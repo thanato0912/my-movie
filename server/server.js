0406a4c9f58ef954ce728f86766869691aef7db9
Sangmin Kim <kim305@usc.edu>
2020-08-04 17:40:04 +0900

    misc

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
