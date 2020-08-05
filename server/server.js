746c0bcd7dc609b5b2ae067fa07987dc4a4e71f5
Sangmin Kim <kim305@usc.edu>
2020-08-05 15:55:18 +0900

    misc

--- server/server.js
+++ server/server.js
@@ -18,11 +18,16 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use('/users', userRouter);
-app.use('/favorites', favoriteRouter);
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  // });
 }
+
+app.use('/users', userRouter);
+app.use('/favorites', favoriteRouter);
 
 mongoose
   .connect(process.env.mongoURI, {
