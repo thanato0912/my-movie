bfa921926c0af8dbe6593e45635f0c9c8d532842
Sangmin Kim <kim305@usc.edu>
2020-08-05 16:25:07 +0900

    misc

--- server/server.js
+++ server/server.js
@@ -21,9 +21,9 @@ app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  // });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 }
 
 app.use('/users', userRouter);
